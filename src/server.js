31cdc4531fc438f54585040db64ce1c0453755a1
Betryz <betryz.f@aluno.ifsp.edu.br>
2024-08-06 15:52:35 -0300

    segundo

--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
-console.log("Olá Mundo!")
+// const express = require('express');
+
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+
+        message: "Olá mundo Express!",
+        success: "Sucesso ao executar API"
+    
+    })
+} )
+
+app.listen(3000, () => {
+    console.log('Servidor Rodando em http://localhost:3000')
+})
+
+
