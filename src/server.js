a7a2ad1a5b0b9ae6447d251fcb0fbc9253c75f62
Betryz <betryz.f@aluno.ifsp.edu.br>
2024-08-20 17:08:12 -0300

    add test undefined env

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@
 import express from 'express'
 import authRouter from './routers/authRouter.js'
 import accountRouter from './routers/accountRouter.js'
+import { ENVIRONMENT } from './config.js';
 
 
 const app = express();
@@ -14,7 +15,7 @@ app.use('/account' , accountRouter)
 
 
 app.listen(3000, () => {
-    console.log(`Servidor Rodando no Ambiente ${ process.env.AMBIENTE}`)
+    console.log(`Servidor Rodando no Ambiente ${ ENVIRONMENT}`)
 })
 
 
