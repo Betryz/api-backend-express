00e3b9e19f2a0872f4617e5f24aded0ca7900778
Betryz <betryz.f@aluno.ifsp.edu.br>
2024-08-20 17:43:01 -0300

    add welcome root

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import { ENVIRONMENT, PORT, HOST } from './config.js';
 
 const app = express();
 
-
+app.get('/' , (req, res) => {res.json({message: "Bem vindo a API"})})
 app.use('/auth', authRouter)
 
 app.use('/account', accountRouter)
