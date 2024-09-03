429fe6f87834b6f6029e4abf84a39051f1c4f3cb
Betryz <betryz.f@aluno.ifsp.edu.br>
2024-09-03 14:58:38 -0300

    add prisma config for local and production

--- src/controllers/account/createController.js
+++ src/controllers/account/createController.js
@@ -3,7 +3,7 @@ import { createAccount } from "../../models/accountModel.js"
 const create = (req, res) => {
     const {service, username, logo_image, pass, user_id} = req.body
 
-    const account = createAccount();
+    const account = createAccount( service, username, logo_image, pass, user_id); 
 
 
 
