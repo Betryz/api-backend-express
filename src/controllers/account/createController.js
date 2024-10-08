bf054b9124b846eb21b8dd5ae584ea45f6eeb9cf
Betryz <beatryz.f@aluno.ifsp.edu.br>
2024-10-08 15:51:24 -0300

    Add zod validation

--- src/controllers/account/createController.js
+++ src/controllers/account/createController.js
@@ -1,10 +1,23 @@
-import { createAccount } from "../../models/accountModel.js"
+import { createAccount, accountValidateToCreate } from "../../models/accountModel.js"
 
 const create = async (req, res, next) => {
     const account = req.body
 
 
     try {
+
+
+        const accountValidated = accountValidateToCreate(account)
+
+        console.log(accountValidated)
+
+        if(!accountValidated.success){
+            return res.status(401).json({
+                error: "Erro ao criar conta!",
+                fieldErrors: accountValidated.error.flatten().fieldErrors
+            })
+
+        }
 
         const result = await createAccount(account);
 
